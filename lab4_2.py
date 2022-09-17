bfc9f6e1bb0b793313bd12b0bb77973c5302eb1f
Богдан Шепель FIT -16 <113444657+BohdanShepel2005@users.noreply.github.com>
2022-09-17 21:26:02 +0300

    Add files via upload

---
+++ lab4_2.py
@@ -0,0 +1,9 @@
+import math
+def func1(x1,x2,x3):
+z=5,126*math.log(3,3+math.pow(x1,2))+math.pow(math.sqrt(x1+math.pow(x2,3)),6)-4*math.tan(x3+x1)
+return(z)
+x=float(input("Введіть x"))
+y=float(input("Введіть y"))
+a=float(input("Введіть a"))
+z=func1(x,y,a)
+print(z)
